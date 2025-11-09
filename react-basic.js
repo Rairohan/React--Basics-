a65dcbb98ca1e0c4b23b2616b6751bb74410429b
rohan <rairohan14303@gmail.com>
2025-11-09 20:34:00 +0545

    first commit

---
+++ react-basic.js
@@ -0,0 +1,4 @@
+const root = document.getElementById("root")
+ReactDOM.createRoot(root).render(<h1>Hello World</h1>)
+//ReactDOM.createRoot(root) act as location in our case root is location
+//render(<h1>Hello World</h1>) acts as what changes is to be done 
