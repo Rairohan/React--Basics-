d23e5ffa660ccf1b49406937b40790998bdda652
rohan <rairohan14303@gmail.com>
2025-11-12 22:09:12 +0545

    Code cleaned

--- ChatBot/Chatbot.js
+++ ChatBot/Chatbot.js
@@ -1,25 +1,51 @@
 //<input /> is a self closing tag
 //<>,</> are fragments which helps to group elements without nesting of div  
-//component uses pascal case capital letter
+//component uses pascal case(Every starting letter is capital letter)
 function ChatInput(){  
     return (
         <>
-            <input placeholder="Send Message" size="25" />
+            <input 
+                placeholder="Send Message"
+                size="25" 
+            />
             <button> Send </button>
         </>
     )
 }
-function ChatMessage(){
+function ChatMessage({message,sender}){
+    // const message = props.message
+    // const sender = props.sender
+    // const {message,sender} = props
+
+   /* if(sender ==="robot"){
+        return(
+            <div>
+                <img src="robot.png" width="50"/>  
+                {message}
+            </div>
+        )
+    } */
+    //&& a guard op where if value 1 is true, value 2 is result
     return(
-        <>
+        < div>
+            {sender ==="robot" && <img src="robot.png" width="50"/> }
+            {message}
+            {sender ==="user" && <img src="user.png" width="50"/> }
             
-        </>
+        </div>
     )
 }
-const app = (
+//conponent reusability message inside Chatmessage is a prop name which is stored in message 
+//variable which is returned 
+function App(){
+    return(
     <>
         <ChatInput />
-    </>
-)
+        <ChatMessage message="Hello chatbot" sender="user"/>
+        <ChatMessage message="Hello! How are you ?" sender="robot"/>
+        <ChatMessage message="Can you get me todays date?" sender="user"/>
+        <ChatMessage message="Today is November 12" sender="robot"/>
+    </>)
+}
 const container = document.getElementById("Container")
-ReactDOM.createRoot(container).render(app)
+ReactDOM.createRoot(container).render(<App/>)
---
+++ Task_2.js
@@ -0,0 +1,20 @@
+function App(){
+    return (
+        <>
+            <p>Hello! Welcome to my Website</p>
+            <LoginForm/>
+            <button>Login</button>
+            <button>SignUP</button>
+        </>
+    )
+}
+function LoginForm(){
+    return (
+    <div>
+                 <input placeholder="email" max="30"/>
+                 <input placeholder="password" max="30"/>
+            </div>
+)
+}
+const location = document.getElementById("Location")
+ReactDOM.createRoot(location).render(<App/>)
